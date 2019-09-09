6351cee7262452b813f32ee47b4983235d23eda1
Hengzhe Ding <916407217@qq.com>
2019-09-08 22:04:35 -0500

    fix response timeout when posting & add readme.md

---
+++ README.md
@@ -0,0 +1,8 @@
+# TE566 Computer Program
+## Overview
+This computer program is a web app for calculating financial statements, it can be 
+run locally.
+## Assignment 1
+1. MySQL is used as database.
+2. Definition of employee, customer, vendor tables are under `./server/models` folder.
+
--- server/index.js
+++ server/index.js
@@ -1,16 +1,60 @@
 const express = require('express');
-const app = express();
 const db = require("./models/index");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const PORT = 81;
 
+const app = express();
+
 app.set("view engine", "ejs");
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.get("/", function (req, res) {
-    res.status(200).json({
-        first_name: "Hengzhe",
-        last_name: "ding"
+    res.send("Welcome to TE566 Computer Program");
+});
+
+app.post("/employee", function(req, res) {
+    let employeeRecord = {
+        FirstName: req.body.FirstName,
+        LastName: req.body.LastName,
+        Address1: req.body.Address1,
+        Address2: req.body.Address2,
+        City: req.body.City,
+        State: req.body.State,
+        ZipCode: req.body.ZipCode,
+        SSN: req.body.SSN,
+        WithHolding: req.body.WithHolding,
+        Salary: req.body.Salary
+    };
+
+    db.query('insert into Employee set ?', employeeRecord, function(err){
+       if (err) {
+           return res.status(400).json({
+               message: err.message
+           })
+       }
+    });
+
+    return res.status(200).json({
+        message: "data inserted successfully"
+    })
+});
+
+app.get("/employee", function(req, res){
+    let selectAllEmployee = "select * from Employee;";
+    db.query(selectAllEmployee, function (err, results){
+        if (err) {
+            return res.status(400).json({
+                message: err.message
+            })
+        } else {
+            return res.status(200).json(results);
+        }
     });
 });
+
 app.listen(PORT, function () {
     console.log(`Server running on port: ${PORT}`)
 });
