16d81e12fb3bdc881388f2138d26f9d4c041a48b
Hengzhe Ding <916407217@qq.com>
2019-10-06 23:21:04 -0500

    add item inspection and insertion for customer and vendor

--- server/index.js
+++ server/index.js
@@ -19,6 +19,14 @@ app.get("/add/employee", function(req, res) {
     res.render("addEmployee");
 });
 
+app.get("/add/customer", function(req, res){
+    res.render("addCustomer");
+});
+
+app.get("/add/vendor", function(req, res){
+    res.render("addVendor");
+});
+
 app.get("/list/employee", function(req, res){
     let selectAll = 'select * from Employee;';
     db.query(selectAll, function(err, result){
@@ -29,6 +37,32 @@ app.get("/list/employee", function(req, res){
        } else {
            res.render('listEmployee', {allEmployee: result});
        }
+    });
+});
+
+app.get("/list/customer", function(req, res){
+    let selectAll = 'select * from Customer;';
+    db.query(selectAll, function(err, result){
+        if (err) {
+            return res.status(400).json({
+                message: err.message
+            });
+        } else {
+            res.render('listCustomer', {allCustomer: result});
+        }
+    });
+});
+
+app.get("/list/vendor", function(req, res){
+    let selectAll = 'select * from Vendor;';
+    db.query(selectAll, function(err, result){
+        if (err) {
+            return res.status(400).json({
+                message: err.message
+            });
+        } else {
+            res.render('listVendor', {allVendor: result});
+        }
     });
 });
 
@@ -57,17 +91,51 @@ app.post("/api/employee", function(req, res) {
     });
 });
 
-app.get("/api/employee", function(req, res){
-    let selectAllEmployee = "select * from Employee;";
-    db.query(selectAllEmployee, function (err, results){
+app.post("/api/customer", function(req, res){
+   let customerRecord = {
+       CompanyName: req.body.CompanyName,
+       LastName: req.body.LastName,
+       FirstName: req.body.FirstName,
+       Address1: req.body.Address1,
+       Address2: req.body.Address2,
+       City: req.body.City,
+       State: req.body.State,
+       ZipCode: req.body.ZipCode,
+       Price: req.body.Price
+   };
+
+   db.query('insert into Customer set ?', customerRecord, function(err){
+       if (err) {
+           return res.status(400).json({
+               message:err.message
+           })
+       } else {
+           res.redirect("/list/customer")
+       }
+   })
+});
+
+app.post("/api/vendor", function(req, res){
+    let vendorRecord = {
+        CompanyName: req.body.CompanyName,
+        Part: req.body.Part,
+        PricePerUnit: req.body.PricePerUnit,
+        Address1: req.body.Address1,
+        Address2: req.body.Address2,
+        City: req.body.City,
+        State: req.body.State,
+        Zip: req.body.Zip
+    };
+
+    db.query('insert into Vendor set ?', vendorRecord, function(err){
         if (err) {
             return res.status(400).json({
-                message: err.message
+                message:err.message
             })
         } else {
-            return res.status(200).json(results);
+            res.redirect("/list/vendor")
         }
-    });
+    })
 });
 
 app.listen(PORT, function () {
